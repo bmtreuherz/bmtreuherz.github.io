9bca9b4be6acb4b2825467592abf92838c392878
Bradley Treuherz <bradleymtreuherz@gmail.com>
2015-08-26 00:36:52 -0400

    Added footer with links to resume, email, and github

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 // invoked on page load
 $(function(){
+  // Begin transitions
   $('#home').addClass("load");
+  $('#footer').addClass("load");
+
+  // Setup event listeners
+  $('[data-toggle="tooltip"]').tooltip({container: 'body'});
 });

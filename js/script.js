a644f0a432522c63f8dd3be244b5c72062aeda0c
Bradley Treuherz <bradleymtreuherz@gmail.com>
2015-08-26 15:40:13 -0400

    Added about me page

--- js/script.js
+++ js/script.js
@@ -6,4 +6,14 @@ $(function(){
 
   // Setup event listeners
   $('[data-toggle="tooltip"]').tooltip({container: 'body'});
+
+  $('#about-me-button').click(function(){
+    $('#home').removeClass('load');
+    $('#about-me').addClass('load');
+  })
+
+  $('#home-button').click(function(){
+    $('#about-me').removeClass('load');
+    $('#home').addClass('load');
+  })
 });

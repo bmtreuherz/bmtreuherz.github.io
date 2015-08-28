a5a22697bf142f306a10d198d4ee5bd1508e8c54
Bradley Treuherz <bradleymtreuherz@gmail.com>
2015-08-27 20:49:33 -0400

    Created basic layout for projects page

--- js/script.js
+++ js/script.js
@@ -5,15 +5,39 @@ $(function(){
   $('#footer').addClass("load");
 
   // Setup event listeners
+  // TODO: Remove tooltip on mobile
   $('[data-toggle="tooltip"]').tooltip({container: 'body'});
 
-  $('#about-me-button').click(function(){
+  // Buttons on home
+  $('#about-from-home').click(function(){
     $('#home').removeClass('load');
     $('#about-me').addClass('load');
   })
 
-  $('#home-button').click(function(){
+  $('#projects-from-home').click(function(){
+    $('#home').removeClass('load');
+    $('#projects').addClass('load');
+  })
+
+  // Buttons on about
+  $('#home-from-about').click(function(){
     $('#about-me').removeClass('load');
     $('#home').addClass('load');
+  })
+
+  $('#projects-from-about').click(function(){
+    $('#about-me').removeClass('load');
+    $('#projects').addClass('load');
+  })
+
+  // Buttons on projects
+  $('#home-from-projects').click(function(){
+    $('#projects').removeClass('load');
+    $('#home').addClass('load');
+  })
+
+  $('#about-from-projects').click(function(){
+    $('#projects').removeClass('load');
+    $('#about-me').addClass('load');
   })
 });
